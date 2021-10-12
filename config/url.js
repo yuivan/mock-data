a96f78b26dfd63c01214c008543eebd579521b91
yu.yifan <yu.yifan@cesgroup.com.cn>
2021-10-12 10:53:36 +0800

    test

--- config/url.js
+++ config/url.js
@@ -2,5 +2,9 @@ const URLS = [];
 const addUrls = (data) => {
   URLS.push(data);
 };
-addUrls({ type: "get", url: "/second", result: { code: "0", data: "second" } });
+addUrls({
+  type: "get",
+  url: "/second",
+  result: { code: "0", data: "second1" },
+});
 module.exports = URLS;
